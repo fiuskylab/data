a87c39d7c3d4cf9f46b5590c7078718aa6a67a02
Rafael <rafael_breno@outlook.com>
2022-09-16 21:39:37 -0300

    feat: Add Append function

--- list/list.go
+++ list/list.go
@@ -3,12 +3,24 @@ package list
 type (
 	// List aka Linked List
 	List[T any] struct {
-		item T
-		next *List[T]
+		value *T
+		next  *List[T]
 	}
 )
 
 // New returns a new List with given type T.
 func New[T any]() *List[T] {
 	return &List[T]{}
+}
+
+// Append will Append a value
+func (l *List[T]) Append(v T) {
+	if l.value == nil {
+		l.value = &v
+		return
+	}
+
+	l.next = &List[T]{
+		value: &v,
+	}
 }
--- list/list_test.go
+++ list/list_test.go
@@ -6,11 +6,31 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestNewList(t *testing.T) {
+func TestNew(t *testing.T) {
 	require := require.New(t)
 
 	expected := List[int]{}
 	actual := New[int]()
 
 	require.Equal(expected, *actual)
+}
+
+func TestAppend(t *testing.T) {
+	list := New[int]()
+
+	t.Run("Empty List", func(t *testing.T) {
+		require := require.New(t)
+		expected := 1
+		list.Append(expected)
+
+		require.Equal(expected, *list.value)
+	})
+
+	t.Run("Non Empty List", func(t *testing.T) {
+		require := require.New(t)
+		expected := 2
+		list.Append(expected)
+
+		require.Equal(expected, *list.next.value)
+	})
 }
