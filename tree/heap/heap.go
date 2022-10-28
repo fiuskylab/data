445d365c198abcf65981d783caa33c7136202c7a
rafaelbreno <rafael_breno@outlook.com>
2022-10-28 18:33:42 -0300

    fix: Remove stale code

--- tree/heap/heap.go
+++ tree/heap/heap.go
@@ -3,20 +3,13 @@ package heap
 type (
 	// Heap - A Tree data structure
 	Heap[T comparable] struct {
-		nodes []node[T]
-	}
-
-	// node - basic unit of data structure.
-	node[T comparable] struct {
-		Value *T
-		Left  *node[T]
-		Right *node[T]
+		nodes []T
 	}
 )
 
 // New returns an empty Heap
 func New[T comparable]() *Heap[T] {
 	return &Heap[T]{
-		nodes: []node[T]{},
+		nodes: []T{},
 	}
 }
